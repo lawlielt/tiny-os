cf43c27b0a8e1366635ce4855b060f770fb9a0ef
lawlielt.liu <lawlielt.liu@gmail.com>
2020-11-20 17:49:37 +0800

    <feature>chapter2

---
+++ chapter2/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Creating disk.img..."
+bximage -mode=create -imgmode="flat" -hd=60M -q disk.img
+
+echo "Compiling..."
+#nasm -I include/ -o mbr.bin mbr.asm
+#nasm -I include/ -o loader.bin loader.asm
+nasm -o mbr.bin mbr.asm
+
+echo "Writing mbr and loader to disk..."
+dd if=mbr.bin of=disk.img bs=512 count=1 conv=notrunc
+#dd if=loader.bin of=disk.img bs=512 count=1 seek=2 conv=notrunc
+
+echo "Now start bochs and have fun!"
+bochs -f bochsrc 
